c53072ce47ac0fa216c48a651e69866ebb99d835
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-04-15 15:45:42 -0400

    added authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import './App.css';
 import { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router'
+import { CheckSession } from './services/Auth'
 
-function App() {
+const App = () => {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const checkToken = async () => {
+    const user = await CheckSession();
+    setUser(user);
+    toggleAuthenticated(true);
+  }
+
+  const handleLogOut = () => {
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return (
     <div className="App">
+      <main>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
