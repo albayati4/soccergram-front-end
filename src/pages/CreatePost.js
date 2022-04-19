8ccef8d280743c5fa61676dbfd5cfd7a91126994
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-04-19 13:46:04 -0400

    updated post form to correct fields

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -2,110 +2,84 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const CreatePost = () => {
-  let navigate = useNavigate()
+    let navigate = useNavigate()
 
-  const [formValues, setFormValues] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
-  })
-
-  const handleChange = (e) => {
-    setFormValues({ ...formValues, [e.target.name]: e.target.value })
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    await RegisterUser({
-      firstName: formValues.firstName,
-      lastName: formValues.lastName,
-      email: formValues.email,
-      password: formValues.password
+    const [formValues, setFormValues] = useState({
+        title: '',
+        image: '',
+        body: ''
     })
-    setFormValues({
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      confirmPassword: ''
-    })
-    navigate('/signin')
 
-  }
+    const handleChange = (e) => {
+        setFormValues({ ...formValues, [e.target.name]: e.target.value })
+    }
 
-  return (
-    <div className="signin col">
-      <div className="card-overlay centered">
-        <form className="col" onSubmit={handleSubmit}>
-          <div className="input-wrapper">
-            <label htmlFor="firstName">First Name</label>
-            <input
-              onChange={handleChange}
-              name="firstName"
-              type="text"
-              placeholder="John"
-              value={formValues.firstName}
-              required
-            />
-          </div>
-          <div className="input-wrapper">
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              onChange={handleChange}
-              name="lastName"
-              type="text"
-              placeholder="Smith"
-              value={formValues.lastName}
-              required
-            />
-          </div>
-          <div className="input-wrapper">
-            <label htmlFor="email">Email</label>
-            <input
-              onChange={handleChange}
-              name="email"
-              type="email"
-              placeholder="example@example.com"
-              value={formValues.email}
-              required
-            />
-          </div>
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await CreatePost({
+            title: '',
+            image: '',
+            body: ''
+        })
+        setFormValues({
+            title: '',
+            image: '',
+            body: ''
+        })
+        navigate('/home')
 
-          <div className="input-wrapper">
-            <label htmlFor="password">Password</label>
-            <input
-              onChange={handleChange}
-              type="password"
-              name="password"
-              value={formValues.password}
-              required
-            />
-          </div>
-          <div className="input-wrapper">
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input
-              onChange={handleChange}
-              type="password"
-              name="confirmPassword"
-              value={formValues.confirmPassword}
-              required
-            />
-          </div>
-          <button
-            disabled={
-              !formValues.email ||
-              (!formValues.password &&
-                formValues.confirmPassword === formValues.password)
-            }
-          >
-            Enter
-          </button>
-        </form>
-      </div>
-    </div>
-  )
+    }
+
+    return (
+        <div className="signin col">
+            <div className="card-overlay centered">
+                <form className="col" onSubmit={handleSubmit}>
+                    <div className="input-wrapper">
+                        <label htmlFor="title">Title</label>
+                        <input
+                            onChange={handleChange}
+                            name="title"
+                            type="text"
+                            placeholder="Messi scores an impossible shot"
+                            value={formValues.title}
+                            required
+                        />
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="image">Image</label>
+                        <input
+                            onChange={handleChange}
+                            name="image"
+                            type="text"
+                            placeholder="Smith"
+                            value={formValues.image}
+                            required
+                        />
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="body">Description</label>
+                        <input
+                            onChange={handleChange}
+                            name="body"
+                            type="text"
+                            placeholder="This goal was insane!"
+                            value={formValues.body}
+                            required
+                        />
+                    </div>
+                    <button
+                        disabled={
+                            !formValues.title ||
+                            !formValues.image ||
+                            !formValues.body
+                        }
+                    >
+                        Enter
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default CreatePost
