02763ecdb40eaf28aaf7d9b52156c2b2b1ae4584
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-04-21 11:58:10 -0400

    added heroku

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import Axios from 'axios'
 export const BASE_URL = 'http://localhost:3001/api'
 let apiUrl = process.env.NODE_ENV === 'production' ? 'https://guarded-ravine-20374.herokuapp.com/' : 'https://localhost:3001/api'
 
-const Client = Axios.create({ baseURL: BASE_URL })
+const Client = Axios.create({ baseURL: apiUrl })
 
 Client.interceptors.request.use(
     (config) => {
